e60f328576bbbf87bbc61ee622f2674fadc35118
Denis Ciccale <dciccale@gmail.com>
2013-05-06 20:33:06 +0300

    add some comments

--- src/andamio.model.js
+++ src/andamio.model.js
@@ -15,18 +15,29 @@ Andamio.Model = Backbone.Model.extend({
     ready: false
   },
 
+  // simple way to know if a model is raedy
+  // usefule for showing a loading component
   isReady: function () {
     return true;
   },
 
+  // shortcut for telling the model is ready
   _setReady: function () {
     this.set('ready', true);
   },
 
+  /* extend model attributes with computed properties
+   * computedProperties: { 
+   *   fullName: function () {
+   *     return this.name + ' ' + this.last;
+   *   }
+   * }
+   */
   _mixinComputedProperties: function () {
     var attributes = this.attributes || {};
     return _.extend(attributes, this.computedProperties);
   },
 
+  // default function to be called from the router recieving url params
   load: function () {}
 });
