cdb921861c79a6e3ea5e9a2b40d3101053cdaa6e
Denis Ciccale <dciccale@gmail.com>
2014-01-22 17:28:45 +0100

    Improve router

--- src/andamio.router.js
+++ src/andamio.router.js
@@ -1,38 +1,28 @@
 Andamio.Router = Backbone.Router.extend({
   constructor: function (options) {
     _.extend(this, options);
-    if (!this.viewsPath) {
-      throw new Error('Provide a correct path for the views in the "viewsPath" option.');
-    }
     this._initRoutes();
     this.initialize.apply(this, arguments);
   },
 
   initialize: function () {},
 
-  // default view files path
-  viewsPath: 'views/',
-
   _initRoutes: function () {
     _.each(this.routes, function (route) {
-      var url = route.url;
-      var view = route.view;
-      var callback = this._routeCallback(url, view);
-      this.route(url, view, callback);
+      var callback = this._routeCallback(route.url, route.name, route.view);
+      this.route(route.url, route.name, callback);
     }, this);
   },
 
-  _routeCallback: function (url, view) {
+  _routeCallback: function (url, name, View) {
     var router = this;
     var callback = function () {
       var urlParams = arguments;
-      require([router.viewsPath + view], function (View) {
-        var _view = new View;
-        if (_view.model && _.isFunction(_view.model.load)) {
-          _view.model.load.apply(_view.model, urlParams);
-        }
-        router.trigger('navigate', _view, url, urlParams);
-      });
+      var view = new View();
+      if (view.model && _.isFunction(view.model.load)) {
+        view.model.load.apply(view.model, urlParams);
+      }
+      router.trigger('navigate', view, url, urlParams);
     };
     return callback;
   }
