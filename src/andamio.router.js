b1611cce3ee216ee05228e3bf2059d14fe00d79b
Denis Ciccale <dciccale@gmail.com>
2014-02-19 09:40:17 +0100

    Add the redirectTo property

--- src/andamio.router.js
+++ src/andamio.router.js
@@ -15,27 +15,36 @@ Andamio.Router = Backbone.Router.extend({
       _.each(urls, function (url) {
 
         // Register the same callback for the same route urls
-        callback = callback || this._createCallback(url, route.name, route.view);
+        callback = callback || this._createCallback(url, route);
         this.route(url, route.name, callback);
       }, this);
 
     }, this);
   },
 
-  _createCallback: function (url, name, View) {
+  _createCallback: function (url, route) {
     var router = this;
+    var callback;
 
-    var callback = function () {
-      var urlParams = arguments;
-      var view = new View();
+    if (route.redirectTo) {
+      callback = function () {
+        Backbone.history.navigate(route.redirectTo, {trigger: true, replace: true});
+      };
+    } else {
+      callback = function () {
+        var urlParams = [].slice.call(arguments);
+        var view = new route.view();
+        var model = view.model || view.collection;
 
-      // Execute view's model load method
-      if (view.model && _.isFunction(view.model.load)) {
-        view.model.load.apply(view.model, urlParams);
-      }
+        // Execute view's model or collection load method
+        if (model && _.isFunction(model.load)) {
+          model.urlParams = urlParams;
+          model.load.apply(model, urlParams);
+        }
 
-      router.trigger('navigate', view, url, urlParams);
-    };
+        router.trigger('navigate', view, url, urlParams);
+      };
+    }
 
     return callback;
   }
